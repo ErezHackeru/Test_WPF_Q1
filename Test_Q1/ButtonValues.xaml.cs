1ee2f4c864a7789d003873ff446deccda750c3d8
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-08-28 21:35:16 +0300

    Add files via upload

---
+++ Test_Q1/ButtonValues.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test_Q1
+{
+    /// <summary>
+    /// Interaction logic for ButtonValues.xaml
+    /// </summary>
+    public partial class ButtonValues : Window
+    {
+        
+        public ButtonValues(Double Width, Double Height, string TextOfBtn)
+        {
+            InitializeComponent();
+            
+            WidthPresentor.DataContext = Width;
+            HeightPresentor.DataContext = Height;
+            TextPresentor.DataContext = TextOfBtn;            
+        }
+        
+        
+    }
+}
---
+++ Test_Q1/MainWinViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Test_Q1
+{
+    public class MainWinViewModel
+    {
+        public DelegateCommand MyDelegate { get; set; }
+
+        public Double Width { get; set; }
+        public Double Height { get; set; }
+        public string TextOfBtn { get; set; }
+
+        public MainWinViewModel()
+        {
+            MyDelegate = new DelegateCommand(ExecuteCommand, CanExecuteMethod);            
+        }
+
+        // =========================== delegate
+        private bool CanExecuteMethod()
+        {
+            return true;
+        }
+
+        private void ExecuteCommand()
+        {
+            
+            ButtonValues BtnValsWin = new ButtonValues(Width, Height, TextOfBtn);
+            BtnValsWin.Show();
+        }
+    }
+}
